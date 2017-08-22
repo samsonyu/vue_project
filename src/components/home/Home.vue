0a58011231cbd359a6ca639cedc2f4c44ce81c23
samson <1243334109@qq.com>
2017-08-22 20:13:34 +0800

    项目搭建

---
+++ src/components/home/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        我是首页
+        <mt-button type="danger" size="large">123</mt-button>
+    </div>
+</template>
+<script type="text/ecmascript-6">
+ export default {
+     data(){
+         return {
+
+         }
+     },created(){
+         this.$ajax.get('getlunbo')
+         .then(res=>{
+             console.log(res);
+         })
+         .catch(err=>{
+             console.log(err);
+         })
+     }
+ }
+</script>
